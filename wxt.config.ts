ff8b8c2643dbc81d21637abaf00aabf92e344482
Mohamed Zumair <mhdzumair@gmail.com>
2026-04-09 06:58:44 +0530

    bump version

--- wxt.config.ts
+++ wxt.config.ts
@@ -29,7 +29,7 @@ export default defineConfig({
     if (manifestVersion === 3) {
       return {
         name: 'APIlot',
-        version: '2.1.0',
+        version: '2.2.0',
         icons,
         permissions: [
           'activeTab',
@@ -49,7 +49,7 @@ export default defineConfig({
     // Firefox MV2
     return {
       name: 'APIlot',
-      version: '2.1.0',
+      version: '2.2.0',
       icons,
       permissions: [
         'activeTab',
